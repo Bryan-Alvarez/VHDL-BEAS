a423325bbef5c25a2b6fdbcafa432a872520446b
Bryan E Alvarez Serna <bemmanuel.alvarez@gmail.com>
2023-02-23 21:29:39 +0000

    Registro de desplazamiento 1 entrada/4salidas

---
+++ Código VHDL/reg_d.vhd
@@ -0,0 +1,33 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY reg_d IS
+PORT(NUM: IN INTEGER RANGE 0 TO 9;
+     CLK: IN STD_LOGIC;
+	  R1, R2, R3, R4: BUFFER INTEGER RANGE 0 TO 9);
+END ENTITY;
+
+ARCHITECTURE BEAS OF reg_d IS
+BEGIN
+
+	PROCESS(CLK) -- Desplazamiento a la izquierda
+	BEGIN
+		IF RISING_EDGE(CLK) THEN
+			R4 <= NUM;
+			R3 <= R4;
+			R2 <= R3;
+			R1 <= R2;
+		END IF;
+	END PROCESS;
+	
+--	PROCESS(CLK) -- Desplazamiento a la derecha
+--	BEGIN
+--		IF RISING_EDGE(CLK) THEN
+--			R1 <= NUM;
+--			R2 <= R1;
+--			R3 <= R2;
+--			R4 <= R3;
+--		END IF;
+--	END PROCESS;
+	
+END BEAS;
